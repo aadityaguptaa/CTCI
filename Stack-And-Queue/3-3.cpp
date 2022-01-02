6430e5c2b2f5166a79e019a7af02dbeaa3115634
Aditya Gupta <52538173+aadityaguptaa@users.noreply.github.com>
2022-01-02 17:18:27 +0530

    traversal done

--- Stack-And-Queue/3-3.cpp
+++ Stack-And-Queue/3-3.cpp
@@ -1,4 +1,149 @@
-//
-// Created by rocko on 26-12-2021.
-//
+#include<bits/stdc++.h>
 
+using namespace std;
+
+class Stack3{
+    int top;
+    int arr[4];
+    int n;
+public:
+    Stack3() {
+        top = -1;
+        n = 4;
+    }
+    bool push(int data){
+        if(top >= n-1){
+            return false;
+        }else{
+            top++;
+            arr[top] = data;
+        }
+        return true;
+    }
+
+    bool isEmpty(){
+        if(top == -1){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    bool pop(){
+        if(!isEmpty()){
+            top--;
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    int peek(){
+        if(!isEmpty()){
+            return arr[top];
+        }
+    }
+};
+
+struct stackNode{
+    class Stack3 stack;
+    struct stackNode *next = nullptr;
+    struct stackNode *prev = nullptr;
+
+};
+
+class SetOfStacks{
+
+    struct stackNode *head = (struct stackNode *) malloc(sizeof (struct stackNode));
+    struct stackNode *temp = head;
+
+public:
+    SetOfStacks(){
+        head -> next = nullptr;
+        head -> prev = nullptr;
+    }
+
+
+    void setPush(int data){
+        bool p = temp->stack.push(data);
+        if(!(p)){
+            struct stackNode *newStack = (struct stackNode *) malloc(sizeof (struct stackNode));
+            newStack -> prev = temp;
+            newStack -> next = nullptr;
+            temp->next = newStack;
+            temp = temp->next;
+            temp->stack.push(data);
+        }
+    }
+
+    bool isSetEmpty(){
+        return head->stack.isEmpty();
+    }
+
+    int setPop(){
+        if(temp->stack.isEmpty() && temp->prev != nullptr){
+            temp = temp -> prev;
+        }
+        int ret = temp->stack.pop();
+
+        return ret;
+    }
+
+    int setPeek(){
+        if(temp->stack.isEmpty() && temp->prev != nullptr){
+            temp = temp->prev;
+        }
+        return temp->stack.peek();
+    }
+
+};
+
+
+int main(void){
+
+    SetOfStacks st = SetOfStacks();
+
+    st.setPush(5);
+    cout << st.setPeek() << endl;
+
+    st.setPush(5);
+    cout << st.setPeek() << endl;
+
+    st.setPush(5);
+    cout << st.setPeek() << endl;
+
+    st.setPush(5);
+    cout << st.setPeek() << endl;
+
+    st.setPush(5);
+    cout << st.setPeek() << endl;
+
+    st.setPush(5);
+    cout << st.setPeek() << endl;
+
+    st.setPush(5);
+    cout << st.setPeek() << endl;
+
+    st.setPush(5);
+    cout << st.setPeek() << endl;
+
+    st.setPop();
+    cout << st.setPeek() << endl;
+    st.setPop();
+    cout << st.setPeek() << endl;
+    st.setPop();
+    cout << st.setPeek() << endl;
+    st.setPop();
+    cout << st.setPeek() << endl;
+    st.setPush(6);
+    cout << st.setPeek() << endl;
+
+    st.setPush(6);
+    cout << st.setPeek() << endl;
+    st.setPush(6);
+    cout << st.setPeek() << endl;
+    st.setPush(6);
+    cout << st.setPeek() << endl;
+    st.setPush(6);
+    cout << st.setPeek() << endl;
+}
--- Trees-And-Graphs/Binary-Tree-Impl.cpp
+++ Trees-And-Graphs/Binary-Tree-Impl.cpp
@@ -1,4 +1,72 @@
-//
-// Created by rocko on 02-01-2022.
-//
+#include<bits/stdc++.h>
 
+using namespace std;
+
+class BSTNode{
+public:
+    int data;
+    BSTNode *left = nullptr;
+    BSTNode *right = nullptr;
+    BSTNode *parent = nullptr;
+};
+
+BSTNode * insert(BSTNode *root, int data){
+
+    if(root == nullptr){
+        root = new BSTNode;
+        root -> data = data;
+        root -> left = nullptr;
+        root -> right = nullptr;
+        root -> parent = nullptr;
+    }else if(root->data < data){
+        root -> right = insert(root->right, data);
+        root->right->parent = root;
+    } else{
+        root -> left = insert(root->left, data);
+        root->left->parent = root;
+    }
+
+    return root;
+}
+
+void inOrder(BSTNode *node){
+    if(node == nullptr){
+        return;
+    }
+    inOrder(node->left);
+    cout << node -> data << endl;
+    inOrder(node->right);
+}
+
+void preOrder(BSTNode *node){
+    if(node == nullptr){
+        return;
+    }
+    cout << node -> data << endl;
+    preOrder(node->left);
+    preOrder(node->right);
+}
+
+void postOrder(BSTNode *node){
+    if(node == nullptr){
+        return;
+    }
+    postOrder(node->left);
+    postOrder(node->right);
+    cout << node -> data << endl;
+
+}
+
+int main(void){
+
+    BSTNode *root = nullptr;
+
+    root = insert(root, 15);
+    root = insert(root, 10);
+    root = insert(root, 20);
+    root = insert(root, 25);
+    root = insert(root, 8);
+    root = insert(root, 12);
+
+    inOrder(root);
+}
